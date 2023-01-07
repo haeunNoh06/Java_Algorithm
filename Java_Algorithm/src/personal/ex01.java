19743d00a1c882c009447306bd3fe19fff98e6d4
haeun Noh <okhagrace06@naver.com>
2023-01-08 00:23:44 +0900

    4x4 입력한 좌표에 X출력

---
+++ Java_Algorithm/src/personal/ex01.java
@@ -0,0 +1,42 @@
+package personal;
+
+import java.util.Scanner;
+
+public class ex01 {
+
+   public static void main(String[] args) {
+
+      Scanner scan = new Scanner(System.in);
+      
+      int row, column;
+      
+      while(true) {
+         System.out.print("행 인덱스 입력 >> ");
+         row = scan.nextInt();
+         System.out.print("열 인덱스 입력 >> ");
+         column = scan.nextInt();
+         
+         if ( row >= 5 || column >= 5) break;
+         
+         System.out.print(" ");
+         for ( int i = 0; i < row; i++)
+            System.out.printf("%2d", i);
+         System.out.println();
+         
+         for ( int i = 0; i < 5; i++) {
+            System.out.print(i);
+            for (int j = 0; j < 5; j++) {
+               System.out.print(" ");
+               if ( row == i && column == j )
+                  System.out.print("X");
+               else
+                  System.out.print(" ");
+            }
+            System.out.println();
+         }
+         System.out.println();
+      }
+      
+      System.out.println("종료되었습니다.");
+   }
+}
