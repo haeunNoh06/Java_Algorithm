934a0ab32d23bd38dfe6b22c051c18cb6c590ad4
haeun Noh <okhagrace06@naver.com>
2023-01-23 22:54:35 +0900

    [프로그래머스] 3진법 뒤집기 pow 사용 전

---
+++ Java_Algorithm/src/programmers/Threejinbub.java
@@ -0,0 +1,61 @@
+package programmers;
+
+public class Threejinbub {
+
+	public int solution(int n) {
+		int answer = 0;
+		
+		String str = "";
+		/*
+        45가 0이 될 때까지 3으로 나눈다
+        45/3 15 0
+        15/3 5 0
+        5/3 1 2
+        1/3 0 1
+        
+        -> 나눈 수를 str에 저장한다
+        -> str의 수를 int로 변환한 뒤 배열에 넣는다
+        -> 각 배열방의 값과 3*(3*i)를 연산하여 더한다
+        -> answer을 리턴한다
+		 */
+		
+		int num = n;
+		while(num != 0) {
+			str += Integer.toString(num%3);
+			num /= 3;
+		}
+		System.out.println(str);
+		
+		String[] arr = str.split("");// 배열로 값 하나씩 담기
+		System.out.println(arr.length);
+		for ( int i = 0; i < arr.length; i++) {
+			System.out.println(arr[i]);
+		}
+		
+		int idx = 0;
+		for (int i = arr.length-1; i >= 0; i--) {
+			num = Integer.parseInt(arr[i]);// 정수로 변환
+			answer += (num*square(idx++));
+			System.out.println("answer : "+answer);
+		}
+		
+		return answer;
+	}
+	
+	public int square(int idx) {
+		int num = 1;
+		for (int i = 1; i <= idx; i++) {
+			num *= 3;
+		}
+		System.out.println("num : "+num);
+		return num;
+	}
+	
+	public static void main(String[] args) {
+
+		Threejinbub tj = new Threejinbub();
+		int num = tj.solution(45);
+		System.out.println("10진수 : "+num);
+	}
+
+}
