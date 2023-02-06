73bb8e2a7be2fec8ed2ac3651732563af7f22625
haeun Noh <okhagrace06@naver.com>
2023-02-06 21:48:13 +0900

    [백준] 10998번_AxB

---
+++ Java_Algorithm/src/baekjoon/b10998.java
@@ -0,0 +1,13 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class b10998 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        
+        int num;
+        num = scan.nextInt();
+        System.out.println(num*scan.nextInt());
+    }
+}
