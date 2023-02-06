af9671f8acce1ee53d4d8540c37d6a772a9e056b
haeun Noh <okhagrace06@naver.com>
2023-02-06 21:44:13 +0900

    [백준] 10926번_??!

---
+++ Java_Algorithm/src/baekjoon/b10926.java
@@ -0,0 +1,12 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class b10926 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        
+        String id = scan.next();
+        System.out.println(id+"??!");
+    }
+}
