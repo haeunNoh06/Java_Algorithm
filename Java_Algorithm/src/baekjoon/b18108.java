8f33cf5034efeda7704d5b239f19ed48d7a851ba
haeun Noh <okhagrace06@naver.com>
2023-02-06 21:48:40 +0900

    [백준] 18108번_1998년생인 내가 태국에서는 2541년생?!

---
+++ Java_Algorithm/src/baekjoon/b18108.java
@@ -0,0 +1,12 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class b18108 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        
+        int year = scan.nextInt();
+        System.out.println(year-543);
+    }
+}
