dfb24f340c7ae4ade7e8d8f4460e66d01d9f36cc
haeun Noh <okhagrace06@naver.com>
2023-02-06 21:48:58 +0900

    [백준] 1008번_A/B

---
+++ Java_Algorithm/src/baekjoon/b1008.java
@@ -0,0 +1,13 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class b1008 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        
+        int num;
+        num = scan.nextInt();
+        System.out.println((double)num/scan.nextInt());
+    }
+}
