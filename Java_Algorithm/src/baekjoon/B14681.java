182e24aa75dd49f6f48b12f2d58c8fbc565b9713
haeun Noh <okhagrace06@naver.com>
2023-02-07 16:52:37 +0900

    [백준] 14681번_사분면 고르기

---
+++ Java_Algorithm/src/baekjoon/B14681.java
@@ -0,0 +1,22 @@
+package baekjoon;
+
+import java.util.Scanner;
+class B14681 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        
+        int X = scan.nextInt();
+        int Y = scan.nextInt();
+        int answer;
+        
+        if ( X > 0) {
+            if ( Y > 0 ) answer = 1;
+            else answer = 4;
+        }
+        else {
+            if ( Y > 0 ) answer = 2;
+            else answer = 3;
+        }
+        System.out.println(answer);
+    }
+} 
