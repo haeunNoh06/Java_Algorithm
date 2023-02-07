8bf4cb0a7b5397b9cc6053bf96b888dd571c06b8
haeun Noh <okhagrace06@naver.com>
2023-02-07 16:29:26 +0900

    [백준] 1330번_두 수 비교하기

---
+++ Java_Algorithm/src/baekjoon/B1330.java
@@ -0,0 +1,18 @@
+package baekjoon;
+
+import java.util.Scanner;
+class B1330 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        
+        int[] num = new int[2];
+        for (int i = 0; i < num.length; i++)
+            num[i] = scan.nextInt();
+        
+        String answer;
+        if (num[0] > num[1]) answer = ">";
+        else if ( num[0] < num[1]) answer = "<";
+        else answer = "==";
+        System.out.println(answer);
+    }
+} 
