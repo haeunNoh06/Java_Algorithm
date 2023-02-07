46a56efc990e764919d31e1b1f8306606eab6a73
haeun Noh <okhagrace06@naver.com>
2023-02-07 17:36:38 +0900

    [백준] 2884번_알람 시계

---
+++ Java_Algorithm/src/baekjoon/B2884.java
@@ -0,0 +1,23 @@
+package baekjoon;
+
+import java.util.Scanner;
+class B2884 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int hour = scan.nextInt();
+        int minute = scan.nextInt();
+     
+        if ( minute-45 < 0 )  {
+            minute = (minute+60)-45;
+            
+            if (hour == 0) {
+                hour = 24;
+            }
+            hour--;
+        }
+        else {
+            minute -= 45;
+        }
+        System.out.println(hour+" "+minute);
+    }
+}
