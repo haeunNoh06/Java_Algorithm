49e851b86ec3404ebcd97855ce2ad045365e791f
haeun Noh <okhagrace06@naver.com>
2023-02-12 22:59:25 +0900

    [백준] 2480번_주사위 세개

---
+++ Java_Algorithm/src/baekjoon/B2480.java
@@ -0,0 +1,36 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class B2480 {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+        
+        int[] A = new int[3];
+        for (int i = 0; i < 3; i++) 
+            A[i] = scan.nextInt();
+        
+        int money = 0;
+        
+        if ( A[0] != A[1] && A[0] != A[2] && A[1] != A[2] ) {
+        	int max = A[0];
+        	for (int i = 1; i < A.length; i++) {
+        		max = Math.max(max, A[i]);
+        	}	
+        	money = max*100;
+        }
+        else if ( A[0] != A[1] || A[0] != A[2] || A[1] != A[2] ) {
+            
+            if (A[0] == A[1])
+                money = 1000+A[0]*100;
+            else if (A[0] == A[2])
+                money = 1000+A[0]*100;
+            else
+                money = 1000+A[1]*100;
+        }
+        else {
+            money = 10000+A[0]*1000;
+        }
+        System.out.println(money);
+    }
+}
