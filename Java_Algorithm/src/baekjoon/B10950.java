a29205b03ab33e989aaff4817f105167b3320a86
haeun Noh <okhagrace06@naver.com>
2023-02-13 13:38:14 +0900

    [백준] 10950번_A+B-3

---
+++ Java_Algorithm/src/baekjoon/B10950.java
@@ -0,0 +1,24 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+class B10950 {
+    
+    public static int plus(int a, int b) {
+        return a+b;
+    }
+    
+    public static void print(int[] a) {
+        for (int i = 0; i < a.length; i++ ) 
+            System.out.println(a[i]);
+    }
+    
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int row = scan.nextInt();
+        int[] A = new int[row];
+        for (int i = 0; i < A.length; i++)
+            A[i] = plus(scan.nextInt(),scan.nextInt());
+        print(A);
+    }
+}
